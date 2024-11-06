7db4f44916c8726de135671385080046908ef6aa
DavidDesk <david.espeton.aparacio@colegiolitterator.com>
2024-11-07 00:24:31 +0100

    Avance jugador contra CPU memory

--- Main.py
+++ Main.py
@@ -1,5 +1,4 @@
 import random
-import copy
 
 print("Bienvenido al juego de Memory")
 
@@ -7,8 +6,7 @@ tableroVacio = []
 tableroEmoji = []
 emojisArray = []
 
-emojis = ["😀", "😁", "😂", "🤣", "🤓", "😄", "😅",
-          "😆", "😎", "😐", "🤑", "😭", "😨", "🤯", "🥵"]
+emojis = ["😀", "😁", "😂", "🤣", "🤓", "😄", "😅", "😆", "😎", "😐", "🤑", "😭", "😨", "🤯", "🥵"]
 
 # ----------------------ETAPA 1: CREACIÓN DEL TABLERO
 crearTablero = False
@@ -19,8 +17,7 @@ while crearTablero == False:
         c = int(input("Introduce el número de columnas (Min = 2x2, Max = 6x5): "))
 
         if (f < 2 or f > 6) or (c < 2 or c > 6) or (f == 6 and c == 6):
-            print(
-                "El número de filas/columnas tiene que estar entre 2 y 6, y no pueden ambas valer 6")
+            print("El número de filas/columnas tiene que estar entre 2 y 6, y no pueden ambas valer 6")
         elif (f * c) % 2 != 0:
             print("El número de filas por columnas debe de ser par")
         else:
@@ -66,10 +63,11 @@ for filas in range(f):
 
 # ------------------ETAPA 2: JUGADOR CONTRA JUGADOR
 print("Tablero creado. ¡Empieza el juego!")
-j1 = input("Introduce el nombre del jugador 1: ")
-j2 = input("Introduce el nombre del jugador 2: ")
 
-def jcj():
+
+def PvP():
+    j1 = input("Introduce el nombre del jugador 1: ")
+    j2 = input("Introduce el nombre del jugador 2: ")
     j1P, j2P = 0, 0
     # ---------------- Bucle de la partida jcj
     while True:
@@ -79,21 +77,18 @@ def jcj():
             try:
                 while True:
                     try:
-                        fc1 = int(
-                            input("Selecciona la fila de la primera carta: ")) - 1
-                        cc1 = int(
-                            input("Selecciona la columna de la primera carta: ")) - 1
+                        print((len(emojisArray)))
+                        fc1 = int(input("Selecciona la fila de la primera carta: ")) - 1
+                        cc1 = int(input("Selecciona la columna de la primera carta: ")) - 1
 
                         if 0 <= fc1 < f and 0 <= cc1 < c:
                             if tableroVacio[fc1][cc1] == "-":
                                 tableroVacio[fc1][cc1] = tableroEmoji[fc1][cc1]
                                 break
                             else:
-                                print(
-                                    "No se puede seleccionar una carta ya desvelada")
+                                print("No se puede seleccionar una carta ya desvelada")
                         else:
-                            print(f"Introduce una posición válida, fila máx: {
-                            f} min: 1 y columna máx: {c} min: 1")
+                            print(f"Introduce una posición válida, fila máx: {f} min: 1 y columna máx: {c} min: 1")
                     except ValueError:
                         print("Posición inválida")
 
@@ -102,21 +97,17 @@ def jcj():
 
                 while True:
                     try:
-                        fc2 = int(
-                            input("Selecciona la fila de la segunda carta: ")) - 1
-                        cc2 = int(
-                            input("Selecciona la columna de la segunda carta: ")) - 1
+                        fc2 = int(input("Selecciona la fila de la segunda carta: ")) - 1
+                        cc2 = int(input("Selecciona la columna de la segunda carta: ")) - 1
 
                         if 0 <= fc2 < f and 0 <= cc2 < c:
                             if tableroVacio[fc2][cc2] == "-":
                                 tableroVacio[fc2][cc2] = tableroEmoji[fc2][cc2]
                                 break
                             else:
-                                print(
-                                    "No se puede seleccionar una carta ya desvelada")
+                                print("No se puede seleccionar una carta ya desvelada")
                         else:
-                            print(f"Introduce una posición válida, fila máx: {
-                            f} min: 1 y columna máx: {c} min: 1")
+                            print(f"Introduce una posición válida, fila máx: {f} min: 1 y columna máx: {c} min: 1")
                     except ValueError:
                         print("Posición inválida")
 
@@ -144,21 +135,17 @@ def jcj():
             try:
                 while True:
                     try:
-                        fc1 = int(
-                            input("Selecciona la fila de la primera carta: ")) - 1
-                        cc1 = int(
-                            input("Selecciona la columna de la primera carta: ")) - 1
+                        fc1 = int(input("Selecciona la fila de la primera carta: ")) - 1
+                        cc1 = int(input("Selecciona la columna de la primera carta: ")) - 1
 
                         if 0 <= fc1 < f and 0 <= cc1 < c:
                             if tableroVacio[fc1][cc1] == "-":
                                 tableroVacio[fc1][cc1] = tableroEmoji[fc1][cc1]
                                 break
                             else:
-                                print(
-                                    "No se puede seleccionar una carta ya desvelada")
+                                print("No se puede seleccionar una carta ya desvelada")
                         else:
-                            print(f"Introduce una posición válida, fila máx: {
-                            f} min: 1 y columna máx: {c} min: 1")
+                            print(f"Introduce una posición válida, fila máx: {f} min: 1 y columna máx: {c} min: 1")
                     except ValueError:
                         print("Posición inválida")
 
@@ -167,21 +154,17 @@ def jcj():
 
                 while True:
                     try:
-                        fc2 = int(
-                            input("Selecciona la fila de la segunda carta: ")) - 1
-                        cc2 = int(
-                            input("Selecciona la columna de la segunda carta: ")) - 1
+                        fc2 = int(input("Selecciona la fila de la segunda carta: ")) - 1
+                        cc2 = int(input("Selecciona la columna de la segunda carta: ")) - 1
 
                         if 0 <= fc2 < f and 0 <= cc2 < c:
                             if tableroVacio[fc2][cc2] == "-":
                                 tableroVacio[fc2][cc2] = tableroEmoji[fc2][cc2]
                                 break
                             else:
-                                print(
-                                    "No se puede seleccionar una carta ya desvelada")
+                                print("No se puede seleccionar una carta ya desvelada")
                         else:
-                            print(f"Introduce una posición válida, fila máx: {
-                            f} min: 1 y columna máx: {c} min: 1")
+                            print(f"Introduce una posición válida, fila máx: {f} min: 1 y columna máx: {c} min: 1")
                     except ValueError:
                         print("Posición inválida")
 
@@ -204,8 +187,130 @@ def jcj():
             break
 
 
-while True:
+# Bucle de la partida de jugador contra la CPU
+def PvAI():
+    j1 = input("Introduce el nombre del jugador 1: ")
+    j1P, cpu = 0, 0
+    # ---------------- Bucle de la partida jcj
+    while True:
+        # Turno del jugador 1
+        while j1P != len(emojisArray):
+            print(f"Turno de {j1}: ")
+            try:
+                while True:
+                    try:
+                        fc1 = int(input("Selecciona la fila de la primera carta: ")) - 1
+                        cc1 = int(input("Selecciona la columna de la primera carta: ")) - 1
 
+                        if 0 <= fc1 < f and 0 <= cc1 < c:
+                            if tableroVacio[fc1][cc1] == "-":
+                                tableroVacio[fc1][cc1] = tableroEmoji[fc1][cc1]
+                                break
+                            else:
+                                print("No se puede seleccionar una carta ya desvelada")
+                        else:
+                            print(f"Introduce una posición válida, fila máx: {f} min: 1 y columna máx: {c} min: 1")
+                    except ValueError:
+                        print("Posición inválida")
+
+                for fila in tableroVacio:
+                    print(" ".join(fila))
+
+                while True:
+                    try:
+                        fc2 = int(input("Selecciona la fila de la segunda carta: ")) - 1
+                        cc2 = int(input("Selecciona la columna de la segunda carta: ")) - 1
+
+                        if 0 <= fc2 < f and 0 <= cc2 < c:
+                            if tableroVacio[fc2][cc2] == "-":
+                                tableroVacio[fc2][cc2] = tableroEmoji[fc2][cc2]
+                                break
+                            else:
+                                print("No se puede seleccionar una carta ya desvelada")
+                        else:
+                            print(f"Introduce una posición válida, fila máx: {f} min: 1 y columna máx: {c} min: 1")
+                    except ValueError:
+                        print("Posición inválida")
+
+                for fila in tableroVacio:
+                    print(" ".join(fila))
+
+                if tableroVacio[fc1][cc1] == tableroVacio[fc2][cc2]:
+                    print("¡Enhorabuena, encontraste una pareja!")
+                    j1P += 2
+                else:
+                    tableroVacio[fc1][cc1] = "-"
+                    tableroVacio[fc2][cc2] = "-"
+                    print("¡Fallaste!")
+                    break
+            except ValueError:
+                print("Introduce una posición válida.")
+
+        if j1P == len(emojisArray):
+            print(f"\nFelicidades {j1}, ganaste la partida con {j1P} puntos")
+            break
+
+        # Turno de la CPU
+        while cpu != len(emojisArray):
+            memoria = []
+            print("Turno de CPU. ")
+            try:
+                while True:
+                    try:
+                        fc1 = random.randint(0, f - 1)
+                        cc1 = random.randint(0, c - 1)
+
+                        if 0 <= fc1 < f and 0 <= cc1 < c:
+                            if tableroVacio[fc1][cc1] == "-":
+                                tableroVacio[fc1][cc1] = tableroEmoji[fc1][cc1]
+                                break
+                            else:
+                                print()
+                        else:
+                            print()
+                    except ValueError:
+                        print("Posición inválida")
+
+                for fila in tableroVacio:
+                    print(" ".join(fila))
+                print()
+
+                while True:
+                    try:
+                        fc2 = random.randint(0, f - 1)
+                        cc2 = random.randint(0, c - 1)
+
+                        if 0 <= fc2 < f and 0 <= cc2 < c:
+                            if tableroVacio[fc2][cc2] == "-":
+                                tableroVacio[fc2][cc2] = tableroEmoji[fc2][cc2]
+                                break
+                            else:
+                                print()
+                        else:
+                            print(f"Introduce una posición válida, fila máx: {f} min: 1 y columna máx: {c} min: 1")
+                    except ValueError:
+                        print("Posición inválida")
+
+                for fila in tableroVacio:
+                    print(" ".join(fila))
+
+                if tableroVacio[fc1][cc1] == tableroVacio[fc2][cc2]:
+                    print("¡Enhorabuena, encontraste una pareja!")
+                    cpu += 2
+                else:
+                    tableroVacio[fc1][cc1] = "-"
+                    tableroVacio[fc2][cc2] = "-"
+                    print("¡Fallaste!")
+                    break
+            except ValueError:
+                print()
+
+        if cpu == len(emojisArray):
+            print(f"\nHa ganado la CPU con {cpu} puntos")
+            break
+
+
+while True:
     print("- Selecciona un modo de juego -")
     print("-------------------------------")
     print("1 - Jugador contra Jugador")
@@ -216,9 +321,9 @@ while True:
 
     match modo:
         case 1:
-            jcj()
+            PvP()
         case 2:
-            print("Hola")
+            PvAI()
         case 3:
             print("Hola")
     break
